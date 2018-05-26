51d46df2033a8d5b9d646612d7e4d82406e9e3ee
thai <thainam83vn@gmail.com>
2018-05-26 05:01:58 -0700

    update

--- import-symbols.js
+++ import-symbols.js
@@ -1,37 +1,33 @@
-const fs = require("fs");
-const csv = require("csvtojson")();
 const cwd = process.cwd();
-
-const files = ["./ref/amex.csv", "./ref/nasdaq.csv", "./ref/nyse.csv"];
-
-async function load() {
-  for (let file of files) {
-    await importSymbols(file);
-    return;
-  }
-}
+let dbo, quotesDb;
 
 require(`${cwd}/helpers/mongodb`)()
-  .then(async db => {
-    dbo = db.db("stocks");
-    const SymbolDb = require(`${cwd}/models/symbols.db`);
-    const symbolsDb = new SymbolDb(dbo);
-    await symbolsDb.drop();
-    for (let file of files) {
-      const json = await csv.fromFile(file);
-      console.log(
-        `-------------------Import ${
-          json.length
-        } rows of ${file}-------------------`
-      );
-      await symbolsDb.insertBatch(file, json);
-      console.log(
-        `-------------------Imported ${
-          json.length
-        } rows of ${file}-------------------`
-      );
-    }
+  .then(db => {
+    console.log("connected to DB");
   })
   .catch(err => console.log(err));
 
-// load();
+const files = ["./ref/amex.csv", "./ref/nasdaq.csv", "./ref/nyse.csv"];
+
+// require(`${cwd}/helpers/mongodb`)()
+//   .then(async db => {
+//     dbo = db.db("stocks");
+//     const SymbolDb = require(`${cwd}/models/symbols.db`);
+//     const symbolsDb = new SymbolDb(dbo);
+//     await symbolsDb.drop();
+//     for (let file of files) {
+//       const json = await csv.fromFile(file);
+//       console.log(
+//         `-------------------Import ${
+//           json.length
+//         } rows of ${file}-------------------`
+//       );
+//       await symbolsDb.insertBatch(file, json);
+//       console.log(
+//         `-------------------Imported ${
+//           json.length
+//         } rows of ${file}-------------------`
+//       );
+//     }
+//   })
+//   .catch(err => console.log(err));
